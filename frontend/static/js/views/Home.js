427cf0c8e66b19dc18e9521696db944029f39e17
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-07-23 18:19:01 +0200

    Small clean up

--- frontend/static/js/views/Home.js
+++ frontend/static/js/views/Home.js
@@ -7,28 +7,6 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
-        // return `
-			
-		// 	<div class="container">
-		// 		<div class="row justify-content-center">
-		// 		<div class="col-12 text-center mb-4">
-		// 			<h1>Title Here</h1>
-		// 		</div>
-		// 		<div class="col-12 text-center mb-2">
-		// 			<a role="button" class="btn btn-primary" href="/pong" data-link>Play</button>
-		// 		</div>
-		// 		<div class="col-12 text-center">
-		// 			<a role="button" class="btn btn-primary" href="/settings data-link>Settings</button>
-		// 		</div>
-		// 		</div>
-		// 	</div>
-        // `;
 		return (await fetch("static/html/home.html")).text();
     }
 }
-
-{/* <nav class="nav">
-	<a href="/" class="nav__link" data-link>Home</a>
-	<a href="/pong" class="nav__link" data-link>Pong</a>
-	<a href="/settings" class="nav__link" data-link>Settings</a>
-</nav> */}
