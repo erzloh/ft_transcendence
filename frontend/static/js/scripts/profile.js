cb0a0d2a6cf1d4d7bdd87dfa9a36fa22ed276501
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-08-16 17:55:21 +0200

    Login, Sign in and profile page works with the backend ! :D

--- frontend/static/js/scripts/profile.js
+++ frontend/static/js/scripts/profile.js
@@ -5,17 +5,26 @@ export async function profile() {
 	console.log('hello from profile')
 	async function renderLoggingInfo() {
 		// Check if the user is logged in or not
-		const response = await fetch(`${BASE_URL}/api/test_token`);
+		const response = await fetch(`${BASE_URL}/api/profile`);
 
-		console.log(response);
-
+		// If the user is not logged in, redirect to the login page
 		if (response.status === 401 || response.status === 400) {
 			navigateTo('/signin');
 		}
 
+		// If the user is logged in, show the profile page
 		if (response.status === 200) {
 			const responseData = await response.json();
 			console.log(responseData);
+
+			const user = responseData.user;
+
+			const usernameElem = document.getElementById('username-name');
+			usernameElem.innerText = user.username;
+
+			const emailElem = document.getElementById('username-email');
+			emailElem.innerText = user.email;
+
 			const profilePage = document.getElementById('profile-page');
 			profilePage.style.display = 'flex';
 		}
@@ -34,8 +43,11 @@ export async function profile() {
 
 	// Log out button
 	const logoutButton = document.querySelector('#logout-button');
-	logoutButton.addEventListener('click', () => {
-		document.cookie = 'session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+	logoutButton.addEventListener('click', async () => {
+		const response = await fetch(`${BASE_URL}/api/logout`, {
+			method: 'POST',
+		});
+		console.log(response);
 		navigateTo('/profile');
 	})
 
