297b4b0f920ca60fb2a54548e0f291ddaaee259b
Yolkers <sacha.ds2002@gmail.com>
2024-08-21 09:30:31 +0200

    almost done refactoring

--- frontend/static/js/scripts/pong/pong.js
+++ frontend/static/js/scripts/pong/pong.js
@@ -20,8 +20,6 @@ export class PongGame {
 		this.endgameModal = new bootstrap.Modal(document.getElementById('endgameModal'));
 
 		this.pWidth = 10, this.pHeight = 100, this.bSize = 10;
-		this.upLeftPressed = false, this.downLeftPressed = false;
-		this.upRightPressed = false, this.downRightPressed = false;
 		this.paused = false;
 
 		const usernamesString = localStorage.getItem('pongUsernames');
@@ -44,15 +42,21 @@ export class PongGame {
 		this.objective = objectiveString ? JSON.parse(objectiveString) : 3;
 		this.objectiveLabel.innerHTML = "points to win: " + this.objective;
 
-		this.leftPad = new Pad(0, this.cvs.height / 2 - this.pHeight / 2, 
-			this.pWidth, this.pHeight, "#FFF", 5);
-		this.rightPad = new Pad(this.cvs.width - this.pWidth, this.cvs.height / 2 - this.pHeight / 2, 
-			this.pWidth, this.pHeight, "#FFF", 5);
-		this.ball = new Ball(this.cvs.width / 2, this.cvs.height / 2,
-			this.bSize, "#FFF", 4, 4, 4 );
-
 		this.boundPongHandleKeyDown = this.handleKeyDown.bind(this);
 		this.boundPongHandleKeyUp = this.handleKeyUp.bind(this);
+		this.boundScorePoint = this.scorePoint.bind(this);
+
+		this.leftPad = new Pad(0, this.cvs.height / 2 - this.pHeight / 2, 
+			this.pWidth, this.pHeight, "#FFF", 5, this.cvs.height -  this.pHeight, 
+			false);
+
+		this.rightPad = new Pad(this.cvs.width - this.pWidth, this.cvs.height / 2 - this.pHeight / 2, 
+			this.pWidth, this.pHeight, "#FFF", 5, this.cvs.height -  this.pHeight,
+			this.gamemode == "AI" ? true : false);
+
+		this.ball = new Ball(this.cvs.width / 2, this.cvs.height / 2,
+			this.bSize, "#FFF", 4, 4, 4, this.cvs.height, this.cvs.width, this.leftPad, this.rightPad, this.boundScorePoint);
+
 		this.gameLoop = this.gameLoop.bind(this);
 	}
 
@@ -82,7 +86,6 @@ export class PongGame {
 		this.endgameModal.show();
 	}
 
-	// TODO: Move those to pads and Ball
 	drawRect(x, y, w, h, color) {
 		this.ctx.fillStyle = color;
 		this.ctx.fillRect(x, y, w, h);
@@ -99,107 +102,23 @@ export class PongGame {
 		}
 	}
 
-	collisionDetect(player, ball) {
-		player.top = player.y;
-		player.bottom = player.y + player.height;
-		player.left = player.x;
-		player.right = player.x + player.width;
-
-		ball.top = ball.y;
-		ball.bottom = ball.y + ball.size;
-		ball.left = ball.x;
-		ball.right = ball.x + ball.size;
-
-		return player.left < ball.right && player.top < ball.bottom && player.right > ball.left && player.bottom > ball.top;
-	}
-
-	// TODO: move this to ball
-	moveBall() {
-		this.ball.x += this.ball.dx;
-		this.ball.y += this.ball.dy;
-
-		if (this.ball.y + this.ball.size > this.cvs.height || this.ball.y < 0) {
-			this.ball.dy *= -1;
-		}
-
-		let currentPlayer = (this.ball.x < this.cvs.width / 2) ? this.leftPad : this.rightPad;
-
-		if (this.collisionDetect(currentPlayer, this.ball)) {
-			this.ball.dx *= -1;
-
-			this.ball.speed += 0.5;
-			if (this.ball.dx > 0) {
-				this.ball.dx = this.ball.speed;
-			} else {
-				this.ball.dx = -this.ball.speed;
-			}
-			if (this.ball.dy > 0) {
-				this.ball.dy = this.ball.speed;
-			} else {
-				this.ball.dy = -this.ball.speed;
-			}
-		}
-
-		if (this.ball.x + this.ball.size > this.cvs.width) {
+	scorePoint = (pad) => {
+		if (pad == "left") {
 			this.leftPad.score++;
 			document.getElementById("leftScore").innerHTML = this.leftPad.score;
-			//playerSc
-            if (this.leftPad.score >= this.objective) {
-                this.endGame(this.usernames.left);
-            }
-			this.resetBall();
-		} else if (this.ball.x < 0) {
+			if (this.leftPad.score >= this.objective) {
+				this.endGame(this.usernames.left);
+			}
+		}
+		else if (pad == "right") {
 			this.rightPad.score++;
 			document.getElementById("rightScore").innerHTML = this.rightPad.score;
-		
             if (this.rightPad.score >= this.objective) {
                 this.endGame(this.usernames.right);
-            }	
-			//computerScore++;
-			this.resetBall();
+            }
 		}
-	}
-
-	// TODO: Move this to ball
-	resetBall() {
-		this.ball.x = this.cvs.width / 2;
-		this.ball.y = this.cvs.height / 2;
-		this.ball.speed = 4;
-		if (Math.random() > 0.5) {
-			this.ball.dx = this.ball.speed;
-		} else {
-			this.ball.dx = -this.ball.speed;
-		}
-		if (Math.random() > 0.5) {
-			this.ball.dy = this.ball.speed;
-		} else {
-			this.ball.dy = -this.ball.speed;
-		}
-	}
-
-	//TODO: Move this to paddle
-	movePaddles() {		
-		if (this.upLeftPressed && this.leftPad.y > 0) {
-			this.leftPad.y -= this.leftPad.dy;
-		} else if (this.downLeftPressed && this.leftPad.y < this.cvs.height - this.leftPad.height) {
-			this.leftPad.y += this.leftPad.dy;
-		}
-		
-		if (this.gamemode == "AI") {
-			if (this.rightPad.y + this.rightPad.height / 2 < this.ball.y) {
-				this.rightPad.y += this.rightPad.dy;
-			} else {
-				this.rightPad.y -= this.rightPad.dy;
-			}
-		}
-		else {
-			if (this.upRightPressed && this.rightPad.y > 0) {
-				this.rightPad.y -= this.rightPad.dy;
-			} else if (this.downRightPressed && this.rightPad.y < this.cvs.height - this.rightPad.height) {
-				this.rightPad.y += this.rightPad.dy;
-			}
-		}
-	}
+		this.ball.resetPosition();
+	}	
 
 	draw() {
 		this.ctx.clearRect(0, 0, this.cvs.width, this.cvs.height);
@@ -232,9 +151,9 @@ export class PongGame {
 
 	update() {
 		if (!this.paused) {
-			this.movePaddles();
-			this.moveBall();
-			//draw();
+			this.leftPad.move();
+			this.rightPad.move();
+			this.ball.move();
 		}
 	}
 
@@ -245,18 +164,22 @@ export class PongGame {
 	}
 
 	handleKeyDown = (event) => {
+		// Prevent buttons from moving the page
+		if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(event.code) > -1) {
+			event.preventDefault();
+		}
 		switch (event.code) {
 			case this.keybinds.lUp:
-				this.upLeftPressed = true;
+				this.leftPad.direction = "up";
 				break;
 			case this.keybinds.lDown:
-				this.downLeftPressed = true;
+				this.leftPad.direction = "down";
 				break;
 			case this.keybinds.rUp:
-				this.upRightPressed = true;
+				this.rightPad.direction = "up";
 				break;
 			case this.keybinds.rDown:
-				this.downRightPressed = true;
+				this.rightPad.direction = "down";
 				break;
 			case 'Escape':
 				if (!this.paused)
@@ -271,16 +194,20 @@ export class PongGame {
 	handleKeyUp = (event) => {
 		switch (event.code) {
 			case this.keybinds.lUp:
-				this.upLeftPressed = false;
+				if (this.leftPad.direction == "up")
+					this.leftPad.direction = "";
 				break;
 			case this.keybinds.lDown:
-				this.downLeftPressed = false;
+				if (this.leftPad.direction == "down")
+					this.leftPad.direction = "";
 				break;
 			case this.keybinds.rUp:
-				this.upRightPressed = false;
+				if (this.rightPad.direction == "up")
+					this.rightPad.direction = "";
 				break;
 			case this.keybinds.rDown:
-				this.downRightPressed = false;
+				if (this.rightPad.direction == "down")	
+					this.rightPad.direction = "";
 				break;
 			default:
 				break;
