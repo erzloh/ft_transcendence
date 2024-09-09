0259da628b1d7da7f01f599d09127f426a019398
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-09-09 13:49:39 +0200

    Add button logic for each stat option

--- frontend/static/js/scripts/pongStatistics.js
+++ frontend/static/js/scripts/pongStatistics.js
@@ -8,10 +8,7 @@ export function pongStatistics () {
 	const aiBtn = document.getElementById('ai-stat-btn');
 	const tournamentBtn = document.getElementById('tournament-stat-btn');
 
-	const statTable = document.getElementById('pong-stat-table')
-
-	// Select the pvp stats by default
-	pvpBtn.classList.add('selected');
+	const statTable = document.getElementById('pong-stat-table');
 
 	// Fill table with pvp stats
 	// Function to fill the table
@@ -27,6 +24,7 @@ export function pongStatistics () {
 			const th = document.createElement('th');
 			th.setAttribute('data-translate', column);
 			tr.appendChild(th);
+			updateTextForElem(th, column);
 		});
 		thead.appendChild(tr);
 		statTable.appendChild(thead);
@@ -35,13 +33,7 @@ export function pongStatistics () {
 		const tbody = document.createElement('tbody');
 		statTable.appendChild(tbody);
 
-		// Get the pvp stats
-		// Make a call to the API to get all the pvp stats
-		// The response will be a JSON array where each element is a pvp stat row.
-		// Map through the response array and add a row to the table for each element of the array.
-		// If there are no pvp stats, display such a message.
-
-		// Get the pvp stats
+		// Get the pvp stats and add them to the table
 		const fillPvpStats = async () => {
 			console.log('Fetching pvp stats');
 			const response = await fetch(`${BASE_URL}/api/PvPong_match_history/`);
@@ -80,11 +72,161 @@ export function pongStatistics () {
 
 	}
 
+	// Fill table with ai stats
+	// Function to fill the table
+	const fillAiTable = () => {
+		// Clear the table
+		statTable.innerHTML = '';
+
+		// Create the table headers
+		const thead = document.createElement('thead');
+		const tr = document.createElement('tr');
+		const columns = ['db date', 'db player', 'db ai lvl', 'db score', 'db winner', 'db duration'];
+		columns.forEach(column => {
+			const th = document.createElement('th');
+			th.setAttribute('data-translate', column);
+			tr.appendChild(th);
+			updateTextForElem(th, column);
+		});
+		thead.appendChild(tr);
+		statTable.appendChild(thead);
+
+		// Create the table body
+		const tbody = document.createElement('tbody');
+		statTable.appendChild(tbody);
+
+		// Get the ai stats and add them to the table
+		const fillAiStats = async () => {
+			console.log('Fetching ai stats');
+			const response = await fetch(`${BASE_URL}/api/AIpong_match_history/`);
+			console.log(response);
+			if (response.status === 200) {
+				const stats = await response.json();
+				console.log(stats);
+
+				// If there are no stats
+				if (stats.length === 0) {
+					const tr = document.createElement('tr');
+					const td = document.createElement('td');
+					td.setAttribute('colspan', '6');
+					td.setAttribute('data-translate', 'no stats');
+					updateTextForElem(td, 'no stats');
+					tr.appendChild(td);
+					tbody.appendChild(tr);
+					return;
+				}
+
+				// Add the stats to the table
+				stats.map(stat => {
+					const tr = document.createElement('tr');
+					const columns = ['match_date', 'player', 'ai_level', 'match_score', 'winner', 'match_duration'];
+					columns.forEach(column => {
+						const td = document.createElement('td');
+						td.textContent = stat[column];
+						tr.appendChild(td);
+					});
+					tbody.appendChild(tr);
+				});
+			}
+		}
+
+		fillAiStats();
+	}
+
+	// Fill table with tournament stats
+	// Function to fill the table
+	const fillTournamentTable = () => {
+		// Clear the table
+		statTable.innerHTML = '';
+
+		// Create the table headers
+		const thead = document.createElement('thead');
+		const tr = document.createElement('tr');
+		const columns = ['db date', 'db player 1', 'db player 2', 'db player 3', 'db player 4', 'db winner', 'db duration'];
+		columns.forEach(column => {
+			const th = document.createElement('th');
+			th.setAttribute('data-translate', column);
+			tr.appendChild(th);
+			updateTextForElem(th, column);
+		});
+		thead.appendChild(tr);
+		statTable.appendChild(thead);
+
+		// Create the table body
+		const tbody = document.createElement('tbody');
+		statTable.appendChild(tbody);
+
+		// Get the tournament stats and add them to the table
+		const fillTournamentStats = async () => {
+			console.log('Fetching tournament stats');
+			const response = await fetch(`${BASE_URL}/api/TournamentPong_match_history/`);
+			console.log(response);
+			if (response.status === 200) {
+				const stats = await response.json();
+				console.log(stats);
+
+				// If there are no stats
+				if (stats.length === 0) {
+					const tr = document.createElement('tr');
+					const td = document.createElement('td');
+					td.setAttribute('colspan', columns.length);
+					td.setAttribute('data-translate', 'no stats');
+					updateTextForElem(td, 'no stats');
+					tr.appendChild(td);
+					tbody.appendChild(tr);
+					return;
+				}
+
+				// Add the stats to the table
+				stats.map(stat => {
+					const tr = document.createElement('tr');
+					const columns = ['match_date', 'player_one', 'player_two', 'player_tree', 'player_four', 'winner', 'match_duration'];
+					columns.forEach(column => {
+						const td = document.createElement('td');
+						td.textContent = stat[column];
+						tr.appendChild(td);
+					});
+					tbody.appendChild(tr);
+				});
+			}
+		}
+
+		fillTournamentStats();
+	}
+
+	// Select the pvp stats by default
+	pvpBtn.classList.add('selected');
 	// Fill the table with pvp stats by default
 	fillPvpTable();	
 
 	// Add Event Listener to each stat button
-	
+	pvpBtn.addEventListener('click', () => {
+		// Remove the selected class from all buttons
+		pvpBtn.classList.add('selected');
+		aiBtn.classList.remove('selected');
+		tournamentBtn.classList.remove('selected');
 
+		// Fill the table with pvp stats
+		fillPvpTable();
+	});
 
+	aiBtn.addEventListener('click', () => {
+		// Remove the selected class from all buttons
+		pvpBtn.classList.remove('selected');
+		aiBtn.classList.add('selected');
+		tournamentBtn.classList.remove('selected');
+
+		// Fill the table with ai stats
+		fillAiTable();
+	});
+
+	tournamentBtn.addEventListener('click', () => {
+		// Remove the selected class from all buttons
+		pvpBtn.classList.remove('selected');
+		aiBtn.classList.remove('selected');
+		tournamentBtn.classList.add('selected');
+
+		// Fill the table with tournament stats
+		fillTournamentTable();
+	});
 }
