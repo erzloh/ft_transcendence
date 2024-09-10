b3bbee74e7c2ed8fed266bad12621b3c890fa7cf
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-09-10 11:39:01 +0200

    Connect pacman stat page to the backend

--- frontend/static/js/scripts/pacmanStatistics.js
+++ frontend/static/js/scripts/pacmanStatistics.js
@@ -1,30 +1,74 @@
+import { BASE_URL } from '../index.js'
+import { updateTextForElem } from '../utils/languages.js';
+
+
 // Function that will be called when the view is loaded
 export function pacmanStatistics () {
-    // const test = "Hello from the pacmanStatistics view!";
-    // console.log(test);
+    // Select tables
+	const pacmanGlobalStatTable = document.getElementById('pacman-global-stat-table');
+	const pacmanHistoryStatTable = document.getElementById('pacman-history-stat-table');
 
-    // // add event listner for keydown
-    // document.addEventListener("keydown", printKey);
+	// Fetch global statistics and fill the global stat table
+	const fillPacmanGlobalStatTable = async () => {
+		const response = await fetch(`${BASE_URL}/api/pacman_stats`);
+		if (response.status === 200) {
+			const responseData = await response.json();
+			const tr = document.getElementById('pacman-global-stat-tbody-tr');
+			const columns = ['total_pacman_matches', 'total_pacman_time', 'max_endless_score']
+			columns.forEach(column => {
+				const td = document.createElement('td');
+				td.textContent = responseData[column];
+				tr.appendChild(td);
+			});
+		}
+	}
 
-    // function testFunction () {
-    //  const test = "this is a test function";
-    //  console.log(test);
-    // }
+	// Fetch history statistics and fill the history table
+	const fillPacmanHistoryStatTable = async () => {
+		const response = await fetch(`${BASE_URL}/api/pacman_matches_history`);
+		if (response.status === 200) {
+			const matches = await response.json();
+			// The JSON is an array of match objects
+			// Get the tbody
+			// Create an array with keys of the JSON object in the order I want to display them in
+			// Map through the array, and create a tr element
+			// go through the keys array, for each item, create a td with its textContent equal to the match[item]
 
-    // testFunction();
+			// Get the tbody from the table
+			const tbody = document.getElementById('pacman-history-stat-tbody');
+
+			// Empty the table
+			tbody.innerHTML = '';
+
+			// Define an array containing keys of a match in the order I want to display them in
+			const columns = ['match_date', 'pacman_player', 'pacman_character', 'ghost_player', 'ghost_character', 'map_name', 'pacman_score', 'winner', 'match_duration'];
+
+			// If there is no history stats, display a custom message
+			if (matches.length === 0) {
+				const tr = document.createElement('tr');
+				const td = document.createElement('td');
+				td.setAttribute('colspan', columns.length);
+				td.setAttribute('data-translate', 'no stats');
+				updateTextForElem(td, 'no stats');
+				tr.appendChild(td);
+				tbody.appendChild(tr);
+				return;
+			}
+
+			// Map through the matches and create a tr filled with td for each match
+			matches.map(match => {
+				const tr = document.createElement('tr');
+				columns.forEach(column => {
+					const td = document.createElement('td');
+					td.textContent = match[column];
+					tr.appendChild(td);
+				});
+				tbody.appendChild(tr);
+			})
+		}
+	}
+
+	// Call the functions to update the tables
+	fillPacmanGlobalStatTable();
+	fillPacmanHistoryStatTable();
 }
-
-// --------------------------- Event Listener Functions ---------------------------
-// (Only Event Listener that are attached to the document.
-// Those attached to elements in the view are gonna be removed
-// when the view changes anyway)
-
-// function printKey (event) {
-//  console.log(event.key);
-
-// }
-
-// --------------------------- Export Event Listeners Object ---------------------------
-// export const eventListeners = {
-//  "keydown": printKey
-// }
